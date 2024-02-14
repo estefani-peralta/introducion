78f25d505b5f3ba87dc21666d5ff2df6d0fb7a5a
estefani-peralta <159305034+estefani-peralta@users.noreply.github.com>
2024-02-14 14:19:05 -0400

    Add files via upload

---
+++ practica02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	char cadena[] = "Hola mundo";
+	char* pc = cadena;
+	int count = 0;  // Variable para contar las vocales en minúscula
+	
+	while (*pc != '\0') {  // Iterar hasta el final de la cadena ('\0')
+		switch (*pc) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			count++;  // Incrementar el contador de vocales en minúscula
+			break;
+		default:
+			break;
+		}
+		pc++;  // Mover el puntero al siguiente carácter en la cadena
+	}
+	
+	cout << "Cadena: " << cadena << endl;
+	cout << "Número de vocales en minúscula: " << count << endl;
+	
+	return 0;
+}
