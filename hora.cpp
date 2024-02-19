d405ab9abbc3bdda1150a74c2194dfeab31bd582
estefani-peralta <159305034+estefani-peralta@users.noreply.github.com>
2024-02-19 11:35:05 -0400

    Add files via upload

---
+++ hora.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+typedef struct {
+	int hora, minuto, segundo;
+} tiempo;
+
+void imprimir(tiempo vt[], int tamanio) {
+	for (int i = 0; i < tamanio; i++) {
+		cout << "Hora " << i + 1 << ": " << vt[i].hora << "." << vt[i].minuto << "." << vt[i].segundo << endl;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	const int TMA = 2;
+	tiempo t1, vt[TMA];
+	t1.hora = 12;
+	t1.minuto = 0;
+	t1.segundo = 0;
+	
+	// Registro array vt
+	vt[0].hora = 13;
+	vt[0].minuto = 30;
+	vt[0].segundo = 10;
+	
+	// Registro array vt 2
+	vt[1].hora = 23;
+	vt[1].minuto = 0;
+	vt[1].segundo = 10;
+	
+	imprimir(vt, TMA);
+	
+	cout << "Hora es: " << t1.hora << "." << t1.minuto << "." << t1.segundo << endl;
+	
+	return 0;
+}
---
+++ horaminutosegundo.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib> // para la función rand()
+#include <ctime>   // para la función time()
+using namespace std;
+
+// Definición de la estructura tiempo
+typedef struct { 
+	int hora, minuto, segundo; 
+} tiempo;
+
+// Función para generar un número aleatorio en el rango [0, 23] para la hora
+int horaRand() {
+	return rand() % 24; // Obtiene un número aleatorio en el rango [0, 23]
+}
+
+// Función para generar un número aleatorio en el rango [0, 59] para los minutos y segundos
+int msRand() {
+	return rand() % 60; // Obtiene un número aleatorio en el rango [0, 59]
+}
+
+// Función para agregar valores aleatorios a los elementos del arreglo vt
+void agregar(tiempo vt[], int tamanio) {
+	for (int i = 0; i < tamanio; i++) {
+		vt[i].hora = horaRand();
+		vt[i].minuto = msRand();
+		vt[i].segundo = msRand();
+	}
+}
+
+// Función para imprimir los elementos del arreglo vt
+void imprimir(tiempo vt[], int tamanio) {
+	for (int i = 0; i < tamanio; i++) {
+		cout << "Hora " << i << ": " << vt[i].hora << "." << vt[i].minuto << "." << vt[i].segundo << endl;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	const int TMA = 15;
+	tiempo t1, vt[TMA];
+	
+	// Inicializa la semilla de generación de números aleatorios
+	srand(time(0));
+	
+	// Agrega valores aleatorios a los elementos del arreglo vt
+	agregar(vt, TMA);
+	
+	// Imprime los elementos del arreglo vt
+	imprimir(vt, TMA);
+	
+	return 0;
+}
